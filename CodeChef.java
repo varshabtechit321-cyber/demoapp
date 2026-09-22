12d0c8b2d9c34130a4dcd3969651a3bfb42001dc
varshabtechit321-cyber <varshabtechit321@gmail.com>
2026-09-22 11:27:32 +0530

    Add files via upload

---
+++ CodeChef.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int result = A * B * 100;
+		System.out.println(result);
+
+	}
+}
